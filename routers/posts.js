99dea193c770dbde82fbabae06f52d835e61f7fc
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-03 13:41:19 +0100

    Show and Create

--- routers/posts.js
+++ routers/posts.js
@@ -7,5 +7,14 @@ router.get(`/`, (req, res) => {
     res.json(posts);
 });
 
+// Show
+router.get(`/:id`, (req, res) => {
+    const postsId = req.params.id;
+    res.json("Prelevo i dettagli del post " + postsId);
+})
 
+// Create
+router.post("", (req, res) => {
+    res.json("Aggiungo un nuovo post");
+})
 module.exports = router;
