87f830a063291cf43e189a5e12364a9fba5c7598
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-03 13:48:19 +0100

    Update and Delete

--- routers/posts.js
+++ routers/posts.js
@@ -11,10 +11,23 @@ router.get(`/`, (req, res) => {
 router.get(`/:id`, (req, res) => {
     const postsId = req.params.id;
     res.json("Prelevo i dettagli del post " + postsId);
-})
+});
 
 // Create
 router.post("", (req, res) => {
     res.json("Aggiungo un nuovo post");
-})
+});
+
+// Update 
+router.put(`/:id`, (req, res) => {
+    const postsId = req.params.id;
+    res.json("Aggiorno tutti i dati del post " + postsId);
+});
+
+// Delete
+router.delete(`/:id`, (req, res) => {
+    const postsId = req.params.id;
+    res.json("Cancellazione del post " + postsId);
+});
+
 module.exports = router;
